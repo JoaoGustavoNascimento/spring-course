c4b8a511616291ef2abd8b3a628f3334bc91e9cf
Gustavo Nascimento <gustavojoao28@gmail.com>
2021-08-23 20:47:35 -0300

    Config lazyloading

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -1,6 +1,5 @@
 package com.springcourse.resource;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,10 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springcourse.domain.Request;
 import com.springcourse.domain.RequestStage;
+import com.springcourse.model.PageModel;
+import com.springcourse.model.PageRequestModel;
 import com.springcourse.service.RequestService;
 import com.springcourse.service.RequestStageService;
 
@@ -44,16 +46,25 @@ public class RequestResource {
 		return ResponseEntity.ok(request);
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<Request>> listAll() {
-		List<Request> requests = requestService.listAll();
-		return ResponseEntity.ok(requests);
+	@GetMapping		
+	public ResponseEntity<PageModel<Request>> listAll(
+			@RequestParam(value = "page") int page,
+			@RequestParam(value = "size") int size) {
+		
+		PageRequestModel pr = new PageRequestModel(page, size);
+		PageModel<Request> pm = requestService.listAllOnLazyModel(pr);
+		
+		return ResponseEntity.ok(pm);
 	}
 	
 	@GetMapping("/{id}/request-stage")
-	public ResponseEntity<List<RequestStage>> listAllStagesById(@PathVariable(name = "id") Long id) {
-		List<RequestStage> stages = stageService.listAllByRequestId(id);
-		return ResponseEntity.ok(stages);
+	public ResponseEntity<PageModel<RequestStage>> listAllStagesById(
+			@PathVariable(name = "id") Long id,
+			@RequestParam(value = "page") int page,
+			@RequestParam(value = "size") int size) {
+		PageRequestModel pr = new PageRequestModel(page, size);
+		PageModel<RequestStage> pm = stageService.listAllByRequestIdOnLazyModel(id, pr);
+		return ResponseEntity.ok(pm);
 	}
 	
 }
--- src/main/java/com/springcourse/service/RequestStageService.java
+++ src/main/java/com/springcourse/service/RequestStageService.java
@@ -5,11 +5,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.springcourse.domain.Request;
 import com.springcourse.domain.RequestStage;
 import com.springcourse.domain.enums.RequestState;
 import com.springcourse.exception.NotFoundException;
+import com.springcourse.model.PageModel;
+import com.springcourse.model.PageRequestModel;
 import com.springcourse.repository.RequestRepository;
 import com.springcourse.repository.RequestStageRepositiry;
 
@@ -42,6 +48,14 @@ public class RequestStageService {
 	public List<RequestStage> listAllByRequestId(Long requestId) {
 		List<RequestStage> stages = requestStageRepositiry.findAllByRequestId(requestId);
 		return stages;
+	}
+	
+	public PageModel<RequestStage> listAllByRequestIdOnLazyModel (Long requestId, PageRequestModel pr) {
+		Pageable pageable = PageRequest.of(pr.getPage(), pr.getSize());
+		Page<RequestStage> page = requestStageRepositiry.findAllByRequestId(requestId, pageable);
+		
+		PageModel<RequestStage> pm = new PageModel<>((int)page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
+		return pm;
 	}
 
 }
